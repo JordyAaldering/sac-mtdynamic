d89688f1717d514eab37dcf8eda906e157722143
Jordy Aaldering <aalderingjordy@gmail.com>
2025-05-27 08:39:52 +0200

    test for performance and efficiency cores

--- scripts_sel265k/bench_power_matmul.sh
+++ scripts_sel265k/bench_power_matmul.sh
@@ -17,37 +17,49 @@ mkdir -p results_sel265k
 
 bench()
 {
-    echo $1 > /sys/class/powercap/intel-rapl:0/constraint_0_power_limit_uw
-    echo $1 > /sys/class/powercap/intel-rapl:0/constraint_1_power_limit_uw
+    echo $2 > /sys/class/powercap/intel-rapl:0/constraint_0_power_limit_uw
+    echo $2 > /sys/class/powercap/intel-rapl:0/constraint_1_power_limit_uw
 
-    numactl --interleave all ./bin/matmul_mt -mt 8 $iter $size \
-        | awk -v size=$size '{
+    numactl -C 0-$(($1-1)) ./bin/matmul_mt -mt 8 $iter $size \
+        | awk -v size=$size -v threads=$1 -v powercap=$2 '{
             wl_idx = (NR - 1) % 2;
-            for (i = 1; i <= NF; i++) {
+            for (i = 3; i <= NF; i++) {
                 b[wl_idx,i] = a[wl_idx,i] + ($i - a[wl_idx,i]) / (NR / 2.0);
                 q[wl_idx,i] += ($i - a[wl_idx,i]) * ($i - b[wl_idx,i]);
                 a[wl_idx,i] = b[wl_idx,i];
             }
         } END {
-            printf "transp,%d", size;
-            for (i = 1; i <= NF; i++) {
+            printf "transp,%d,%d,%d", size, threads, powercap;
+            for (i = 3; i <= NF; i++) {
                 printf ",%f,%f", a[0,i], sqrt(q[0,i] / (NR / 2.0));
             }
             print "";
-            printf "matmul,%d", size;
-            for (i = 1; i <= NF; i++) {
+            printf "matmul,%d,%d,%d", size, threads, powercap;
+            for (i = 3; i <= NF; i++) {
                 printf ",%f,%f", a[1,i], sqrt(q[1,i] / (NR / 2.0));
             }
             print "";
         }' >> "results_sel265k/matmul.csv"
 }
 
-bench  15000000
-bench  25000000
-bench  35000000
-bench  45000000
-bench  55000000
-bench  65000000
-bench  75000000
-bench 100000000
-bench 125000000
+# Use only performance cores
+bench 8  15000000
+bench 8  25000000
+bench 8  35000000
+bench 8  45000000
+bench 8  55000000
+bench 8  65000000
+bench 8  75000000
+bench 8 100000000
+bench 8 125000000
+
+# Use both performance and efficiency cores
+bench 20  15000000
+bench 20  25000000
+bench 20  35000000
+bench 20  45000000
+bench 20  55000000
+bench 20  65000000
+bench 20  75000000
+bench 20 100000000
+bench 20 125000000
--- scripts_sel265k/bench_power_matmul_naive.sh
+++ scripts_sel265k/bench_power_matmul_naive.sh
@@ -17,31 +17,43 @@ mkdir -p results_sel265k
 
 bench()
 {
-    echo $1 > /sys/class/powercap/intel-rapl:0/constraint_0_power_limit_uw
-    echo $1 > /sys/class/powercap/intel-rapl:0/constraint_1_power_limit_uw
+    echo $2 > /sys/class/powercap/intel-rapl:0/constraint_0_power_limit_uw
+    echo $2 > /sys/class/powercap/intel-rapl:0/constraint_1_power_limit_uw
 
-    numactl --interleave all ./bin/matmul_naive_mt -mt 8 $iter $size \
-        | awk -v size=$size '{
-            for (i = 1; i <= NF; i++) {
+    numactl -C 0-$(($1-1)) ./bin/matmul_naive_mt -mt 8 $iter $size \
+        | awk -v size=$size -v threads=$1 -v powercap=$2 '{
+            for (i = 3; i <= NF; i++) {
                 b[i] = a[i] + ($i - a[i]) / NR;
                 q[i] += ($i - a[i]) * ($i - b[i]);
                 a[i] = b[i];
             }
         } END {
-            printf "matmul_naive,%d", size;
-            for (i = 1; i <= NF; i++) {
+            printf "matmul_naive,%d,%d,%d", size, threads, powercap;
+            for (i = 3; i <= NF; i++) {
                 printf ",%f,%f", a[i], sqrt(q[i] / NR);
             }
             print "";
         }' >> "results_sel265k/matmul_naive.csv"
 }
 
-bench  15000000
-bench  25000000
-bench  35000000
-bench  45000000
-bench  55000000
-bench  65000000
-bench  75000000
-bench 100000000
-bench 125000000
+# Use only performance cores
+bench 8  15000000
+bench 8  25000000
+bench 8  35000000
+bench 8  45000000
+bench 8  55000000
+bench 8  65000000
+bench 8  75000000
+bench 8 100000000
+bench 8 125000000
+
+# Use both performance and efficiency cores
+bench 20  15000000
+bench 20  25000000
+bench 20  35000000
+bench 20  45000000
+bench 20  55000000
+bench 20  65000000
+bench 20  75000000
+bench 20 100000000
+bench 20 125000000
