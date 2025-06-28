bc4185f634c21b8c07ac45a839aff550134b34e4
Jordy Aaldering <aalderingjordy@gmail.com>
2025-06-28 13:14:50 +0200

    nbody test seq

--- scripts_sel265k/power_nbody.sh
+++ scripts_sel265k/power_nbody.sh
@@ -10,7 +10,8 @@ fi
 iter=$1
 size=$2
 
-make bin/nbody_mt || exit 1
+make bin/nbody_seq || exit 1
+make bin/nbody_mt  || exit 1
 
 mkdir -p results_sel265k
 
@@ -19,8 +20,8 @@ bench()
     echo $2 > /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw
     echo $2 > /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_power_limit_uw
 
-    numactl -C 0-$(($1-1)) ./bin/nbody_mt -mt $1 $iter $size \
-        | awk -v size=$size -v threads=$1 -v powercap=$2 '{
+    numactl -C 0-$(($1-1)) $1 $2 $iter $size \
+        | awk -v size=$size -v threads=$2 -v powercap=$3 '{
             for (i = 3; i <= NF; i++) {
                 b[i] = a[i] + ($i - a[i]) / NR;
                 q[i] += ($i - a[i]) * ($i - b[i]);
@@ -35,24 +36,30 @@ bench()
         }' >> "results_sel265k/nbody.csv"
 }
 
+# Single-threaded
+for w in 15000000 25000000 35000000 45000000 55000000 65000000 75000000 100000000 125000000
+do
+    bench "./bin/nbody_seq" 1 $w
+end
+
 # Use only performance cores
-bench 8  15000000
-bench 8  25000000
-bench 8  35000000
-bench 8  45000000
-bench 8  55000000
-bench 8  65000000
-bench 8  75000000
-bench 8 100000000
-bench 8 125000000
+bench "./bin/nbody_mt -mt " 8  15000000
+bench "./bin/nbody_mt -mt " 8  25000000
+bench "./bin/nbody_mt -mt " 8  35000000
+bench "./bin/nbody_mt -mt " 8  45000000
+bench "./bin/nbody_mt -mt " 8  55000000
+bench "./bin/nbody_mt -mt " 8  65000000
+bench "./bin/nbody_mt -mt " 8  75000000
+bench "./bin/nbody_mt -mt " 8 100000000
+bench "./bin/nbody_mt -mt " 8 125000000
 
 # Use both performance and efficiency cores
-bench 20  15000000
-bench 20  25000000
-bench 20  35000000
-bench 20  45000000
-bench 20  55000000
-bench 20  65000000
-bench 20  75000000
-bench 20 100000000
-bench 20 125000000
+bench "./bin/nbody_mt -mt " 20  15000000
+bench "./bin/nbody_mt -mt " 20  25000000
+bench "./bin/nbody_mt -mt " 20  35000000
+bench "./bin/nbody_mt -mt " 20  45000000
+bench "./bin/nbody_mt -mt " 20  55000000
+bench "./bin/nbody_mt -mt " 20  65000000
+bench "./bin/nbody_mt -mt " 20  75000000
+bench "./bin/nbody_mt -mt " 20 100000000
+bench "./bin/nbody_mt -mt " 20 125000000
