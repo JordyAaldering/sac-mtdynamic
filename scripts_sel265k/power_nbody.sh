223a0380a02bd82c63b0796842b7697b4d8706f0
Jordy Aaldering <aalderingjordy@gmail.com>
2025-06-28 13:42:49 +0200

    stress

--- scripts_sel265k/power_nbody.sh
+++ scripts_sel265k/power_nbody.sh
@@ -32,6 +32,7 @@ bench()
 }
 
 for threads in 1 8 20; do
+  stress --cpu 20 --timeout 60
   for size in 10000 25000; do
     for power in 10000000 15000000 25000000 35000000 45000000 55000000 65000000 75000000 100000000 125000000; do
       bench $threads $size $power
