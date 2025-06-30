0537dded9e7535b1d9287cefc71b4a8402063a8c
Jordy Aaldering <aalderingjordy@gmail.com>
2025-06-30 13:29:32 +0200

    smaller rust tests

--- scripts_sel265k/power_rust_matmul_transp.sh
+++ scripts_sel265k/power_rust_matmul_transp.sh
@@ -18,7 +18,7 @@ bench()
     echo $power > /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw
     echo $power > /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_power_limit_uw
 
-    numactl -C 0-$(($threads-1)) ./src_rust/target/release/matmul_transp $ITER $size $threads \
+    numactl -C 0-$(($threads-1)) ./src_rust/target/release/matmul-transp $ITER $size $threads \
         | awk -v size=$size -v threads=$threads -v powercap=$power '{
             for (i = 2; i <= NF; i++) {
                 b[i] = a[i] + ($i - a[i]) / NR;
