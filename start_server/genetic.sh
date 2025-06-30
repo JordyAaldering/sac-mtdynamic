d948b6c80c85a6ef84bdafc384f409a10d7327f6
Jordy Aaldering <aalderingjordy@gmail.com>
2025-06-30 09:43:59 +0200

    resource controller additional config options

--- start_server/genetic.sh
+++ start_server/genetic.sh
@@ -1,6 +1,8 @@
 mtdynamic --once \
           --letterbox-size 20 \
           genetic --score energy \
+                  --power-rate-min 0.05 \
+                  --threads-rate-min 1 \
                   --survival-rate 0.25 \
                   --mutation-rate 0.30 \
                   --immigration-rate 0
