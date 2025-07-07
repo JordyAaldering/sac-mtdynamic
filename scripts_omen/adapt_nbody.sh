e693cef394d79782e7daae98f9e2ad72e5d3e22e
Jordy Aaldering <aalderingjordy@gmail.com>
2025-07-07 13:36:44 +0200

    adaptation script

---
+++ scripts_omen/adapt_nbody.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+    printf "Usage: %s <iter> <size>\n" "$0" >&2
+    printf "\t<iter>: number of times to repeat the experiment\n" >&2
+    printf "\t<size>: input matrix size\n" >&2
+    exit 1
+fi
+
+iter=$1
+size=$2
+
+make bin/nbody_mtd || exit 1
+
+# Warmup
+numactl --interleave all -C 0,2,4,6 ./bin/nbody_mtd -mt 4 10 $size
+
+./start_server/genetic.sh &
+sleep 1 # Wait for server to be ready
+numactl --interleave all -C 0,2,4,6 ./bin/nbody_mtd -mt 4 $iter $size
